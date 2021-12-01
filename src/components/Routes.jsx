894c3049ba1621539aed58836d5cec17bb07ee54
truongnhatnguyen <truongnhatnguyen9999@gmail.com>
2021-12-01 17:15:53 +0700

    add-barChart

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { Route, Switch } from 'react-router-dom'
 
-import Dashboard from '../pages/Dashboard'
-import Customers from '../pages/Customers'
-import BarChart from '../pages/BarChart'
+import Dashboard from 'pages/Dashboard'
+import Customers from 'pages/Customers'
+import BarChart from 'pages/BarChart'
 
 const Routes = () => {
     return (
